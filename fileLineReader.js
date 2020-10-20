cb28ff10d127d1c49d0ab85ede973a6d3b1483e3
Dzmitry Naumau <d.naumau@godeltech.com>
2020-10-20 21:21:27 +0200

    add init commit

---
+++ fileLineReader.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const fs = require('fs');
+const proccessor = require('./processorv2');
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('./file1.smile'),
+    console: false
+});
+
+readInterface.on('line', function(line) {
+    proccessor.processLine(line)
+});
+readInterface.on('close', function(line) {
+    console.log(JSON.stringify(proccessor.getResult(),null,4))
+});
